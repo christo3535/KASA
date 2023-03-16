daf128ccf56984770334c95e570cba7e62b47349
christotchalakov <christo3535@hotmail.com>
2023-03-16 17:28:12 +0100

    ajout de bannerH

--- src/components/Banner_home/BannerHome.js
+++ src/components/Banner_home/BannerHome.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import "./banner_home.css"
+import BannerImg from "@/images/bannerHome.jpg"
+
 const BannerHome = () => {
     return (
         <div className='banner-home'>
-            BannerHome
+            <img className={"bannerHome"} src={BannerImg} alt="Vue de mer, falaises et forêt"/>
+            <div className='bannerText'>
+              <p>Chez vous, partout et ailleurs</p>
+            </div>
         </div>
     );
 };
