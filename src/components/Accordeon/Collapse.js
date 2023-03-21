3af7423b7e5d431e7c6257c71f9b9ec91f0764d7
christotchalakov <christo3535@hotmail.com>
2023-03-21 12:59:40 +0100

    collapse texr

--- src/components/Accordeon/Collapse.js
+++ src/components/Accordeon/Collapse.js
@@ -1,9 +1,35 @@
 import React from 'react';
+import { useState } from 'react';
+import ArrowUp from "../../images/arrow.png";
+import  ArrowDown from "../../images/arrow_back.png"
 
-const Collapse = () => {
+
+const Collapse = ({title, content}) => {
+
+const [toggle, setToggle] = useState(false);
+
+const handleToggle = () => {
+    setToggle(!toggle);
+};
+
+
     return (
-        <div>
-            
+        <div className='collapse'>
+            <h2 className='collapse_title' onClick={handleToggle}>{title}
+            {
+               toggle ? <img className='arrow' src= { ArrowUp }  alt="arrow"/>  : <img className='arrow' src={ ArrowDown} alt="arrow"/>
+
+            }
+            </h2>
+
+            <div className={ toggle ? 'collapse_content' : 'collapse_hiden'}>
+               {  Array.isArray(content) ? content.map((element, index) => {
+                   return (<p key={index}>{element}</p>)
+               }) : content } 
+
+            </div>
+ 
+
         </div>
     );
 };
