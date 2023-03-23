318f25bbcd7137785aea70edb63f405debc2fa79
christotchalakov <christo3535@hotmail.com>
2023-03-23 19:17:05 +0100

    mis en place caroussel.js

---
+++ src/components/Caroussel/Caroussel.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { useState } from 'react';
+import ArrowRight from '../../images/arrow_forward.png'
+import ArrowLeft from '../../images/arrowleft.png'
+import './caroussel.css'
+
+
+const Caroussel = ({picturesCaroussel}) => {
+
+    const [index, setIndex] = useState(0)
+    
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Caroussel;
