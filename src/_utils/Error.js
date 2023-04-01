798436f6eca852d580e63865a301b5665ff483e0
christotchalakov <christo3535@hotmail.com>
2023-04-01 19:11:44 +0200

    margin dans error

--- src/_utils/Error.js
+++ src/_utils/Error.js
@@ -8,7 +8,7 @@ const Error = () => {
         <div className='Error'>
             <h1 className='error-title'>404</h1>
             <h2 className='error_info'>Oups!La page que <span>vous demandez n'existe pas!</span></h2>
-            <Link  className='backLink' to='/'>Retournez sur la page d'accueil!</Link>
+            <Link  className='backLink' to='/'>Retournez sur la page d'accueil</Link>
         </div>
     );
 };
